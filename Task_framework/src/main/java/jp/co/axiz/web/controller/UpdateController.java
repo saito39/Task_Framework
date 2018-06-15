7d2fc5962e0cce03c133b8931e6a4a1e0f1944b7
admin <admin@admin-HP>
2018-06-15 13:33:05 +0900

    180615-3

--- Task_framework/src/main/java/jp/co/axiz/web/controller/UpdateController.java
+++ Task_framework/src/main/java/jp/co/axiz/web/controller/UpdateController.java
@@ -1,7 +1,5 @@
 package jp.co.axiz.web.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,16 +7,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jp.co.axiz.web.dao.UserInfoDao;
-import jp.co.axiz.web.entity.Update;
+import jp.co.axiz.web.entity.Session;
+import jp.co.axiz.web.entity.UserInfo;
 import jp.co.axiz.web.form.UpdateForm;
+import jp.co.axiz.web.service.Impl.UserInfoServiceImpl;
 
 @Controller
 public class UpdateController {
 
 	@Autowired
-	private UserInfoDao updDao;
-	private Update afterUser;
+	//private UserInfoDao updDao;
+	//private Update afterUser;
+	private Session session;
+
+	@Autowired
+	private UserInfoServiceImpl uiServiceImpl;
 
 	@RequestMapping(value="/update",method=RequestMethod.GET)
 	public String update(@ModelAttribute("updateForm") UpdateForm form,Model model) {
@@ -27,49 +30,98 @@ public class UpdateController {
 
 	@RequestMapping(value="/updateInput", method=RequestMethod.POST)
 	public String updateInput(@ModelAttribute("updateForm") UpdateForm form,Model model) {
-		model.addAttribute("updId",form.getUpdId());
 
-		String updId = form.getUpdId();
+		//model.addAttribute("updId",form.getUpdId());
+		//String updId = form.getUpdId();
 
-		if(updId == null || updId.isEmpty()) {
+		if(userId == null || updId.isEmpty()) {
 			model.addAttribute("errmsg","必須項目を入力してください");
 			return "update";
 		}
 
-		List<Update> updList = updDao.findAll();
+		//List<Update> updList = updDao.findAll();
+		UserInfo user = UserInfoServiceImpl.findById(form.getUserId());
 
-		for(Update update : updList) {
+		/*for(Update update : updList) {
 			 if(!(updId.equals(update.getId()))) {
 				model.addAttribute("errmsg","入力されたデータは存在しません");
 				return "update";
-			}
+			}*/
 
-			afterUser.setId(update.getId());
-			afterUser.setName(update.getName());
-			afterUser.setTelephone(update.getTelephone());
-			afterUser.setPassword(update.getPassword());
-
-			model.addAttribute("afterUser",afterUser);
+		if(user==null) {
+			model.addAttribute("errmsg","入力されたデータは存在しません");
+			return "update";
 		}
+
+		session.setPrevUser(user);
+
+		form.setNewName(user.getUserName());
+		form.setNewTel(user.getTelephone());
+		form.setNewPassword(user.getPassword());
+
+		//model.addAttribute("afterUser",afterUser);
 		return "updateInput";
 	}
 
 	@RequestMapping(value="/updateConfirm",method=RequestMethod.POST)
 	public String updateConfirm(@ModelAttribute("updateForm") UpdateForm form,Model model) {
-/*
+
+
+		/*
 		if( 1項目も変更されてなかったら ) {
 			model.addAttribute("errmsg","1項目以上変更してください");
 			return "updateInput";
 
 		} else {*/
-			return "updateConfirm";
+
+		if (form.hasError()) {
+			model.addAttribute("errmsg", "1項目以上変更してください");
+			return "updateInput";
 		}
-	//}
 
-		@RequestMapping(value="/update",method=RequestMethod.POST)
-		public String success(@ModelAttribute("updateForm") UpdateForm form,Model model) {
+		UserInfo beforeUser = session.getPrevUser();
 
-			/*if( pass不一致 ) {
+		UserInfo afterUser = new UserInfo();
+		afterUser.setUserId(beforeUser.getUserId());
+		afterUser.setUserName(form.getNewName());
+		afterUser.setTelephone(form.getNewTel());
+		afterUser.setPassword(form.getNewPassword());
+
+		if(afterUser.equals(beforeUser)) {
+			model.addAttribute("errmsg", "1項目以上変更してください");
+			return "updateInput";
+		}
+
+		session.setAfterUser(afterUser);
+
+		form.setPrevName(beforeUser.getUserName());
+		form.setPrevTel(beforeUser.getTelephone());
+		form.setPrevPassword(beforeUser.getPassword());
+
+		if(beforeUser.getPassword().equals(afterUser.getPassword())) {
+			form.setConfirmNewPassword(afterUser.getPassword());
+		}
+
+		return "updateConfirm";
+	}
+
+	@RequestMapping(value = "/updateInputBack")
+	public String updateInputBack(@ModelAttribute("updateForm") UpdateForm form, Model model) {
+
+		UserInfo afterUser = session.getAfterUser();
+
+		form.setUserId(afterUser.getUserId());
+		form.setNewName(afterUser.getUserName());
+		form.setNewTel(afterUser.getTelephone());
+		form.setNewPassword(afterUser.getPassword());
+
+		return "updateInput";
+	}
+
+	@RequestMapping(value="/update",method=RequestMethod.POST)
+	public String success(@ModelAttribute("updateForm") UpdateForm form,Model model) {
+
+		/*if( pass不一致 ) {
 				model.addAttribute("errmsg","前画面で入力されたパスワードと一致しません");
 				return "updateConfirm";
 
@@ -80,7 +132,30 @@ public class UpdateController {
 
 				model.addAttribute("updId", afterUser.getId());*/
 
-				return "updateResut";
-			}
+
+		UserInfo afterUser = session.getAfterUser();
+
+		if(!afterUser.getPassword().equals(form.getConfirmNewPassword())) {
+			model.addAttribute("errmsg", "前画面で入力されたパスワードと一致しません");
+
+			form.setConfirmNewPassword("");
+
+			UserInfo beforeUser = session.getPrevUser();
+			form.setPrevName(beforeUser.getUserName());
+			form.setPrevTel(beforeUser.getTelephone());
+			form.setPrevPassword(beforeUser.getPassword());
+
+			return "updateConfirm";
 		}
-		//}
+
+		userInfoService.update(afterUser);
+
+		session.setAfterUser(null);
+		session.setPrevUser(null);
+
+		model.addAttribute("user", session.getLoginUser());
+
+		return "updateResut";
+	}
+}
+
